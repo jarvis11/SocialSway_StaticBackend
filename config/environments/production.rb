11f66600fdc221a74710a93a80af8bf782d30d66
Bhaskar Ravi <bvravi11@gmail.com>
2013-10-23 17:19:39 -0500

    removed SSL for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ SocialSwayTimemachine::Application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx, default rails is commented out
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  #config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
